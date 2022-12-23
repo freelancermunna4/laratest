ff1569e8859d6d66c40ae3df1953c6d939b368d9
user.email <freelancermunna4@gmail.com>
2022-12-23 11:53:26 +0600

    My first Laravel Project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="margin:50px;text-align:center">
+    <?php echo $Peter ?>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis laboriosam in, debitis provident minus quo architecto perspiciatis praesentium aliquam, eius illum sapiente? Nihil, quis. Obcaecati, accusamus ipsum. Error autem provident at sapiente aliquid, dolores, minus sed praesentium aperiam animi laborum nisi dignissimos adipisci, facere eum quas inventore cumque id veritatis. Dolore enim, quod totam dolor aperiam esse alias dolores delectus, facere ipsa molestias, commodi nostrum numquam voluptates consequatur ea illum accusamus dignissimos? Enim itaque quisquam dolor pariatur, impedit harum earum at ipsa natus ratione labore atque tenetur autem et. Accusantium, a neque eius consequuntur ex odio soluta assumenda, sint saepe eligendi perferendis maiores aperiam dolor laudantium repellat temporibus minus? Maiores sed, nesciunt nihil omnis sint exercitationem iste, soluta, officia ducimus possimus vitae perspiciatis dolor veniam eaque libero eligendi. Eos ipsum dolore impedit aperiam inventore fugiat fuga soluta reprehenderit cumque! Ipsum nihil sequi possimus ipsam officia, fugit ullam explicabo reprehenderit labore cum, maxime vel sunt iste nostrum? Rlumem quibusdam quo exercitationem optio. Obcaecati iure dolores aliquam eos deserunt officia quos, sequi tenetur enim eveniet fugiat delectus? Quam officiis, omnis saepe ut totam nobis magni odio reiciendis sapiente et, fuga cumque iure distinctio maxime quidem dolorem ducimus rem? Est il omnis adipisci tempora odio incidunt deleniti dolorem dolore, harum itaque dolorum modi, enim ipsa veritatis officia unde quasi ratione optio illo delectus. Vero quibusdam, tempora, laudantium voluptas mollitia reiciendis quae quo in facere, rerum nisi doloremque inventore dignissimos maxime pariatur voluptatem molestias dolorem non obcaecati distinctio sint fugiat! Eaque, ex quisquam. Sequi ipsa aspernatur alias sunt? Alias qui quod error eligendi quos eos quibusdam quae ipsam ducimus omnis? Ex eius velit quasi aliquid animi quisquam consequuntur repellendus. Neque minus accusamus eveniet voluptatem reprehenderit magni sit aliquid cumque iste maiores eligendi aut blanditiis, itaque, fuga dolorum dolor sint quia ad! Dolorem labore veniam exercitationem! Reiciendis aspernatur temporibus debitis eos maiores. Culpa odit ipsa at voluptatibus iste, blanditiis impedit commodi officiis hic voluptatum quo recusandae reiciendis, consequatur nemo architecto odio ipsum fugit molestias nulla labore aliquid numquam temporibus? Deserunt, maiores. Asperiores facere in, magni quasi facilis porro ducimus, accusamus labore temporibus suscipit quaerat. Id dignissimos, explicabo delectus ipsa ea eligendi iste est soluta aspernatur nesciunt corporis blanditiis? Sit debitis accusantium quibusdam soluta aperiam incidunt eaque, non laboriosam optio repellat, iure consequuntur nisi quo, velit rem ullam beatae eveniet nemo. Error aspernatur ipsam animi molestias cum quod minima cupiditate at ex harum! Animi quasi, blanditiis sapiente eum tenetur repellat id eos mollitia error vel at reiciendis assumenda officiis exercitationem. Provident quo eius beatae amet culpa saepe ducimus. Accusantium cumque in deserunt officia, reprehenderit a temporibus iste, beatae animi, vel ipsa numquam optio ipsam provident reiciendis ab voluptatibus quos quis? Quas eveniet delectus dolorem impedit, harum, tempore consequuntur qui iste reprehenderit doloribus veniam porro accusamus, tenetur eligendi consectetur ipsum obcaecati cum quaerat nisi repellat mollitia perferendis quam libero! Doloremque repudiandae, harum fugiat cumque, nisi recusandae autem vero magni molestias dolores optio voluptatem sit inventore ipsum nulla earum repellat aliquid! Iusto fugit sapiente repellat quia odio facilis!
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('contact', function () {
+    return "This is contact Pages";
+});
